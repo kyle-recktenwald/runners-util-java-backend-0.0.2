e856e110be50b9c00ffaf4b4fc2862bffffcadee
Kyle Recktenwald <kreck30590@gmail.com>
2024-07-02 15:59:11 -0400

    Test Coverage Attempt

---
+++ src/test/java/com/krecktenwald/runnersutil/controllers/RunControllerTest.java
@@ -0,0 +1,14 @@
+package com.krecktenwald.runnersutil.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RunControllerTest {
+
+    @Test
+    void testGetRuns() {
+        assertTrue(true);
+    }
+
+}
