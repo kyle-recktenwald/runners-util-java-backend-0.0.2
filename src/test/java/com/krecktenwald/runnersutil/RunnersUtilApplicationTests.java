449b8fc6038bb1618294a1b9135b05bd901d34f7
Kyle Recktenwald <kreck30590@gmail.com>
2024-07-02 13:31:30 -0400

    Disabling Local Context Load Test

--- src/test/java/com/krecktenwald/runnersutil/RunnersUtilApplicationTests.java
+++ src/test/java/com/krecktenwald/runnersutil/RunnersUtilApplicationTests.java
@@ -1,11 +1,13 @@
 package com.krecktenwald.runnersutil;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RunnersUtilApplicationTests {
 
+  @Disabled("Disabled until embedded database is set up.")
   @Test
   void contextLoads() {}
 }
