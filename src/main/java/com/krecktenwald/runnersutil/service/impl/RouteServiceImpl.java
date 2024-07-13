fe930dd667710ae8d3bc711eb00683021e9a586d
Kyle Recktenwald <kreck30590@gmail.com>
2024-07-13 06:00:36 -0400

    Cleanup

--- src/main/java/com/krecktenwald/runnersutil/service/impl/RouteServiceImpl.java
+++ src/main/java/com/krecktenwald/runnersutil/service/impl/RouteServiceImpl.java
@@ -16,7 +16,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RouteServiceImpl implements RouteService {
 
   private static final Logger logger = LogManager.getLogger(RouteServiceImpl.class);
